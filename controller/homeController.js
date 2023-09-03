38ae91a5156a374e5726d7749d3315375a47c3f6
Bhavesh Singh <bhaveshsinghbiz@gmail.com>
2023-09-03 20:16:21 +0530

    section 08 done

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,13 +1,15 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middleware/bigPromise");
+
+exports.home = BigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Hello from API",
   });
-};
+});
 
-exports.homeDummy = (req, res) => {
+exports.homeDummy = BigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Dummy thic",
   });
-};
+});
